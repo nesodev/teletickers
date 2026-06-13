464c487416e34e1eff4de831f223f94e5e75092c
Ever Frank Avendaño Meza <ever.avendano@unmsm.edu.pe>
2026-06-13 09:30:00 -0500

    allow any origin

--- src/cors.rs
+++ src/cors.rs
@@ -1,15 +1,10 @@
 use actix_cors::Cors;
 use actix_web::http::header;
 
-pub fn configure_cors(frontend_url: &str) -> Cors {
+pub fn configure_cors(_frontend_url: &str) -> Cors {
     Cors::default()
-        .allowed_origin(frontend_url)
-        .allowed_origin("http://localhost:4321")
-        .allowed_origin("http://127.0.0.1:4321")
-        .allowed_origin("http://localhost:3000")
-        .allowed_origin("http://127.0.0.1:3000")
+        .allow_any_origin()
         .allowed_methods(vec!["GET", "POST", "PUT", "DELETE", "OPTIONS"])
         .allowed_headers(vec![header::AUTHORIZATION, header::ACCEPT, header::CONTENT_TYPE])
-        .supports_credentials()
         .max_age(3600)
 }
